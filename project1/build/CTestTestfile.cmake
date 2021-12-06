f650bbfec9dc61aef5acb8482ed45236ad665a1e
ldzjas <ldzjas@126.com>
2021-12-06 15:09:56 +0800

    提交

--- project1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/sql/scudb_initial
-# Build directory: /mnt/d/sql/scudb_initial/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
